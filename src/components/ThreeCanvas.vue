a50fb3dfcbdd556b1e55d1b910b2d60ffb7c49fe
Vikms95 <vms1995@hotmail.com>
2022-12-12 08:23:11 +0100

    show mesh and rotate it

--- src/components/ThreeCanvas.vue
+++ src/components/ThreeCanvas.vue
@@ -1,64 +1,89 @@
-<style scoped>
+<style>
 
+  canvas {
+    width: 100%;
+    height: 100%;
+  }
 
 </style>
+
 
 <script setup>
 
 import * as THREE from 'three';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
+import { ref, watch, computed, onMounted } from 'vue';
+import {useWindowSize} from '@vueuse/core';
 
-// Canvas
-const canvas = document.querySelector( 'canvas.webgl' );
 
-// Scene
+let renderer;
+let controls;
+const {width, height} = useWindowSize();
+const aspectRatio = computed( () => width.value / height.value );
+const experience = ref( null );
 const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 
+	75, 
+	aspectRatio.value, 
+	0.01,
+	1000
+);
 
-/**
-   * Objects
-   */
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 'white' } );
+function updateRenderer ( ) {
+	renderer.setSize( width.value, height.value );
+	renderer.setPixelRatio( window.devicePixelRatio );
+}
+
+function updateCamera ( ) {
+	camera.aspect = aspectRatio.value;
+	camera.updateProjectionMatrix;
+}
+
+watch( aspectRatio, updateRenderer );
+watch( aspectRatio, updateCamera );
+
+const geometry = new THREE.SphereGeometry( 1.5, 30 );
+const material = new THREE.MeshBasicMaterial( { color: 'yellow' } );
 const mesh = new THREE.Mesh( geometry, material );
+
+camera.position.z = 5;
 scene.add( mesh );
-
-/**
-   * Sizes
-   */
-const sizes = {
-	width: 800,
-	height: 600
-};
-
-/**
-   * Camera
-   */
-const camera = new THREE.PerspectiveCamera( 75, sizes.width / sizes.height );
-camera.position.z = 3;
 scene.add( camera );
 
 
-/**
-   * Renderer
-   */
-const renderer = new THREE.WebGLRenderer( {
-	canvas: canvas,
-	antialias: true
-} );
+const clock = new THREE.Clock();
 
-renderer.setSize( sizes.width, sizes.height );
+const loop = () => {
+	const elapsedTime = clock.getElapsedTime();
+
+	renderer.render( scene, camera );
+	mesh.position.x = Math.sin( elapsedTime * 0.5 ) * 5;
+	mesh.position.z = Math.cos( elapsedTime * 0.5 ) * 5; 
+	requestAnimationFrame( loop );
+};
 
 
-const props = defineProps( {
-	id: String,
+onMounted( () => {
+	renderer = new THREE.WebGLRenderer( {
+		canvas: experience.value,
+		antialias: true,
+	} );
+
+	controls = new OrbitControls( camera, renderer.domElement );
+	controls.enableDamping = true;
+	controls.enablePan = true;
+	updateRenderer();
+	updateCamera;
+	loop();
+
+	// const controls = new OrbitControls( camera, renderer );
+	renderer.setSize( width.value, height.value );
 } );
 
 </script>
 
-
 <template>
-
-  <canvas v-bind:id='props.id' class='webgl'></canvas>
-
+  <canvas ref='experience'/>
 </template>
 
 
