80a23712980eef83826b476e8a2022a69cdb9a5a
Vikms95 <vms1995@hotmail.com>
2022-12-21 08:46:14 +0100

    apply planets rotation

--- src/components/ThreeCanvas.vue
+++ src/components/ThreeCanvas.vue
@@ -11,10 +11,10 @@
 <script setup>
 
 import * as THREE from 'three';
-// import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
 import { 
 	ref,
-	// watch,
+	watch,
 	computed,
 	onMounted }
 	from 'vue';
@@ -36,7 +36,6 @@ import neptuneTexture from '../assets/neptune.jpg';
 import plutoTexture from '../assets/pluto.jpg';
 
 let renderer;
-console.log( 'test out scope' );
 const {width, height} = useWindowSize();
 const aspectRatio = computed( () => width.value / height.value );
 
@@ -48,7 +47,7 @@ const camera = new THREE.PerspectiveCamera(
 	0.1,
 	1000
 );
-camera.position.set( 0, 0, 20 );
+camera.position.set( -90, 140, 140 );
 scene.add( camera );
 
 let sun;
@@ -64,21 +63,21 @@ let pluto;
 
 
 
-// function updateRenderer ( ) {
-// 	renderer.setSize( width.value, height.value );
-// 	renderer.setPixelRatio( window.devicePixelRatio );
-// }
+function updateRenderer ( ) {
+	renderer.setSize( width.value, height.value );
+	renderer.setPixelRatio( window.devicePixelRatio );
+}
 
-// function updateCamera ( ) {
-// 	camera.aspect = aspectRatio.value;
-// 	camera.updateProjectionMatrix;
-// }
+function updateCamera ( ) {
+	camera.aspect = aspectRatio.value;
+	camera.updateProjectionMatrix;
+}
 
-// watch( aspectRatio, updateRenderer );
-// watch( aspectRatio, updateCamera );
+watch( aspectRatio, updateRenderer );
+watch( aspectRatio, updateCamera );
 
 const ambientLight = new THREE.AmbientLight( 0x333333 );
-const pointLight = new THREE.PointLight( 0xFFFFFF, 2, 300 );
+const pointLight = new THREE.PointLight( 0xFFFFFF, 2, 200 );
 scene.add( pointLight );
 scene.add( ambientLight );
 
@@ -121,53 +120,49 @@ function createPlanet ( size, texture, position, ring ) {
 
 function animate () {
 	//Self-rotation
-	// sun.rotateY( 0.004 );
-	// mercury.mesh.rotateY( 0.004 );
-	// venus.mesh.rotateY( 0.002 );
-	// earth.mesh.rotateY( 0.02 );
-	// mars.mesh.rotateY( 0.018 );
-	// jupiter.mesh.rotateY( 0.04 );
-	// saturn.mesh.rotateY( 0.038 );
-	// uranus.mesh.rotateY( 0.03 );
-	// neptune.mesh.rotateY( 0.032 );
-	// pluto.mesh.rotateY( 0.008 );
+	sun.rotateY( 0.0001 );
+	mercury.mesh.rotateY( 0.004 );
+	venus.mesh.rotateY( 0.002 );
+	earth.mesh.rotateY( 0.002 );
+	mars.mesh.rotateY( 0.0018 );
+	jupiter.mesh.rotateY( 0.004 );
+	saturn.mesh.rotateY( 0.0038 );
+	uranus.mesh.rotateY( 0.003 );
+	neptune.mesh.rotateY( 0.0032 );
+	pluto.mesh.rotateY( 0.0008 );
 
 	// //Around-sun-rotation
-	// mercury.obj.rotateY( 0.04 );
-	// venus.obj.rotateY( 0.015 );
-	// earth.obj.rotateY( 0.01 );
-	// mars.obj.rotateY( 0.008 );
-	// jupiter.obj.rotateY( 0.002 );
-	// saturn.obj.rotateY( 0.0009 );
-	// uranus.obj.rotateY( 0.0004 );
-	// neptune.obj.rotateY( 0.0001 );
-	// pluto.obj.rotateY( 0.00007 );
+	mercury.obj.rotateY( 0.0004 );
+	venus.obj.rotateY( 0.00015 );
+	earth.obj.rotateY( 0.0001 );
+	mars.obj.rotateY( 0.00008 );
+	jupiter.obj.rotateY( 0.00002 );
+	saturn.obj.rotateY( 0.000009 );
+	uranus.obj.rotateY( 0.000004 );
+	neptune.obj.rotateY( 0.000001 );
+	pluto.obj.rotateY( 0.0000007 );
 	
 	requestAnimationFrame( animate );
 	renderer.render( scene, camera );
-	console.log( 'render', scene );
-	console.log( 'camera', camera );
 }
 
-// window.addEventListener( 'resize', function () {
-// 	camera.aspect = window.innerWidth / window.innerHeight;
-// 	camera.updateProjectionMatrix();
-// 	renderer.setSize( window.innerWidth, window.innerHeight );
-// } );
+window.addEventListener( 'resize', function () {
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+	renderer.setSize( window.innerWidth, window.innerHeight );
+} );
 
 onMounted( () => {
 	renderer = new THREE.WebGLRenderer( {
 		canvas: experience.value,
 	} );
-  
-	console.log( 'test in scope' );
-  
-	// const controls = new OrbitControls( camera, renderer.domElement );
-	// controls.update();
-	// updateRenderer();
-	// updateCamera();
-  
 	renderer.setSize( width.value, height.value );
+	updateRenderer();
+	updateCamera();
+
+	const controls = new OrbitControls( camera, renderer.domElement );
+	controls.update();
+  
   
 	const cubeTextureLoader = new THREE.CubeTextureLoader();
 	scene.background = cubeTextureLoader.load( [
@@ -178,32 +173,35 @@ onMounted( () => {
 		starsTexture,
 		starsTexture
 	] );
-
-	const sunGeo = new THREE.SphereGeometry( 2, 30, 30 );
+  
+	const sunGeo = new THREE.SphereGeometry( 40, 30, 30 );
 	const sunMat = new THREE.MeshBasicMaterial( {map: textureLoader.load( sunTexture )} );
 	sun = new THREE.Mesh( sunGeo, sunMat );
+	scene.add( sun );
 
-	createPlanet( 1.5, mercuryTexture, 4 );
-	createPlanet( 1.8, venusTexture, 5 );
-	createPlanet( 1, earthTexture, 6 );
-	createPlanet( 1, marsTexture, 7 );
-	createPlanet( 1, jupiterTexture, 10 );
-	createPlanet( 1, saturnTexture, 13, {
+	mercury = createPlanet( 3.2, mercuryTexture, 68 );
+	venus = createPlanet( 5.8, venusTexture, 90 );
+	earth = createPlanet( 6, earthTexture, 162 );
+	mars = createPlanet( 4, marsTexture, 278 );
+	jupiter = createPlanet( 12, jupiterTexture, 400 );
+	saturn = createPlanet( 10, saturnTexture, 538, {
 		innerRadius: 10,
 		outerRadius: 20,
 		texture: saturnRingTexture
 	} );
 
-	createPlanet( 1, uranusTexture, 176, {
+	uranus = createPlanet( 7, uranusTexture, 776, {
 		innerRadius: 7,
 		outerRadius: 12,
 		texture: uranusRingTexture
 	} );
-	createPlanet( 1, neptuneTexture, 200 );
-	createPlanet( 1, plutoTexture, 216 );
 
-	scene.add( sun );
-  
+	uranus.obj.rotation.x = 2;
+
+	neptune = createPlanet( 7, neptuneTexture, 1000 );
+
+	pluto = createPlanet( 2.8, plutoTexture, 1216 );
+
 	animate();
 } );
 
