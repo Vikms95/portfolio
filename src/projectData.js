089f9ac879274211712a1f65c873bf392ec9b50b
Vikms95 <vms1995@hotmail.com>
2023-04-23 10:06:21 +0200

    refactor: :lipstick: add projects

--- src/projectData.js
+++ src/projectData.js
@@ -3,27 +3,9 @@ const path = '/tech-icons/'
 
 export const projectsData = [
   {
-    name: 'Storyvine',
-    description:
-      'Social network to share and rate your experiences with literature. Ability to create lists of books, keep track of the progress and rate them',
-    images: '/project-images/codespot-img.jpg',
-    technologies: [icons.ts, icons.react, icons.node, icons.mongo],
-    githubLink: 'https://github.com/Vikms95/storyvine',
-    projectLink: '',
-  },
-  {
-    name: 'Errorlab',
-    description:
-      'Social network to share and rate your experiences with literature. Ability to create lists of books, keep track of the progress and rate them',
-    images: '/project-images/codespot-img.jpg',
-    technologies: [icons.ts, icons.react, icons.node, icons.mongo],
-    githubLink: 'https://github.com/Vikms95/errorlab',
-    projectLink: '',
-  },
-  {
     name: 'Codespot',
     description:
-      'Social network to share and rate your experiences with literature. Ability to create lists of books, keep track of the progress and rate them.',
+      "Social network to share and rate your experiences. Ability to interact with other users by commenting each other's posts",
     images: '/project-images/codespot-img.jpg',
     technologies: [icons.ts, icons.react, icons.node, icons.mongo],
     githubLink: 'https://github.com/Vikms95/blog-api',
@@ -37,5 +19,23 @@ export const projectsData = [
     technologies: [icons.ts, icons.react, icons.node, icons.mongo],
     githubLink: 'https://github.com/Vikms95/react-wheres-waldo',
     projectLink: 'https://vikms95.github.io/react-wheres-waldo/',
+  },
+  {
+    name: 'VGKeys',
+    description:
+      'Videogame digital key webpage, where you can filter videogames provided by an API. ',
+    images: '/project-images/vg-keys.jpg',
+    technologies: [icons.ts, icons.react],
+    githubLink: 'https://github.com/Vikms95/react-shop-cart',
+    projectLink: 'https://vikms95.github.io/react-shop-cart/',
+  },
+  {
+    name: 'Memolearn',
+    description:
+      'Memory card game where time a card is clicked, the player scores.',
+    images: '/project-images/memolearn.jpg',
+    technologies: [icons.ts, icons.react],
+    githubLink: 'https://github.com/Vikms95/react-memory-card',
+    projectLink: 'https://vikms95.github.io/react-memory-card/',
   },
 ]
